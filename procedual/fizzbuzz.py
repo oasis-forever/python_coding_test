de26126238232dffc3aebd2c0cda52a8d0cbe8ba
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-06-25 22:33:57 +0900

    Commit procedual dir

---
+++ procedual/fizzbuzz.py
@@ -0,0 +1,14 @@
+# for i in range(1, 101):
+#     if i % 3 == 0 and i % 5 == 0:
+#         result = "FizzBuzz"
+#     elif i % 3 == 0:
+#         result = "Fizz"
+#     elif i % 5 == 0:
+#         result = "Buzz"
+#     else:
+#         result = str(i)
+#     print(result)
+#
+# for i in range(1, 101):
+#     result = "FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i)
+#     print(result)
---
+++ procedual/nabeatsu.py
@@ -0,0 +1,12 @@
+# # Express the status of "crazy" with '!'
+#
+# for i in range(1, 41):
+#     if i % 3 == 0 or "3" in str(i):
+#         result = str(i) + "!"
+#     else:
+#         result = str(i)
+#     print(result)
+#
+# for i in range(1, 41):
+#     result = str(i) + "!" if i % 3 == 0 or "3" in str(i) else str(i)
+#     print(result)
