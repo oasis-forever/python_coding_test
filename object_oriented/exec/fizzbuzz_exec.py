fbd6f1781903c6c26e4dc14d7e0d0f192a09a035
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-11-20 17:45:24 +0900

    Update dir structure

--- object_oriented/exec/fizzbuzz_exec.py
+++ object_oriented/exec/fizzbuzz_exec.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("./lib")
+sys.path.append("../lib")
 from fizzbuzz import FizzBuzz
 
 fizzbuzz = FizzBuzz()
--- object_oriented/exec/nabeatsu_exec.py
+++ object_oriented/exec/nabeatsu_exec.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("./lib")
+sys.path.append("../lib")
 from nabeatsu import Nabeatsu
 
 nabeatsu = Nabeatsu()
